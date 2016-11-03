e1f6a61824ce58d69dc6de283e9df00fa1d9a8d7
kingmac <17mccowkind@washk12.org>
2016-11-02 20:49:22 -0600

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,4 +1,15 @@
+import random
+COLORS = ['yellow', 'red', 'blue', 'green']
+
 class Monster:
+  min_hit_points = 1
+  max_hit_points = 1
+  min_exp = 1
+  max_exp = 1
+  weapon = "sword"
+  sound = "roar
+  
+  
   def__init__(Self, **kwargs):
     self.hit_points = kwargs.get('hit_points', 1)
     self.weapon = kwargs.get('weapon', 'sword')
